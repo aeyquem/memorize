cebb95e21c06081d97fbdf787f00ca0cc8e3c19b
Alejandro Eyquem <fabriziorossi@vivaldi.net>
2023-09-27 15:47:14 -0300

    Created repository with project files

---
+++ README.md
@@ -0,0 +1,37 @@
+# Memorize
+
+This is the game Memorize, where you need to find pairs of matching cards.
+
+## Project Setup
+
+Make sure you have node v20.6 installed, then install the dependencies with
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+you shold be able to load the project in your browser by going to the URL:
+
+```txt
+http://localhost:5173
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+## Live version
+
+The live version of the proyect is hosted on:
+
+```txt
+https://memorize-ae.netlify.app
+```
---
+++ src/components/Card/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <figure class="w-24 h-36 rounded-lg">
+    <Transition name="flip" mode="out-in">
+      <img
+        v-if="isFlipped"
+        :src="card.src"
+        :alt="card.slug"
+        :title="card.slug"
+        class="w-full h-full object-cover rounded-lg border-[1px] border-amber-300"
+      />
+      <img
+        v-else
+        class="bg-[url('@/assets/card.webp')] w-full h-full rounded-lg border-[1px] border-white-200"
+      />
+    </Transition>
+  </figure>
+</template>
+
+<script setup>
+defineProps({
+  card: {
+    src: String,
+    slug: String,
+  },
+  isFlipped: Boolean,
+});
+</script>
+<style scoped>
+.flip-enter-from,
+.flip-leave-to {
+  transform: rotateY(90deg);
+}
+
+.flip-enter-active,
+.flip-leave-active {
+  transition: all 0.1s ease;
+}
+</style>
